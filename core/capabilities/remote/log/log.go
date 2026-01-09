02b6948ad1d1c294d5f2b1e93dc54d4ae510f0fd
Matthew Pendrey <matthew.pendrey@gmail.com>
2026-01-09 14:33:57 +0000

    tidy

--- core/capabilities/remote/log/log.go
+++ core/capabilities/remote/log/log.go
@@ -1,7 +1,12 @@
 package log
 
+import (
+	"encoding/hex"
+	"unicode"
+)
+
 // TODO this is duplicated from utils to minimise the PR changeset - will be deduped later
-/*
+
 const (
 	maxLoggedStringLen = 256
 )
@@ -19,4 +24,3 @@ func SanitizeLogString(s string) string {
 	}
 	return s + tooLongSuffix
 }
-*/
