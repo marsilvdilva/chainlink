cbe24a7ced9174e4dfd219764b8d3d46b4160d70
Lautaro Fernandez <juan.lautarofernandez@smartcontract.com>
2026-01-20 16:32:07 -0300

    testing fix for ccip integration tests

--- core/services/ocr2/plugins/ccip/testhelpers/integration/chainlink.go
+++ core/services/ocr2/plugins/ccip/testhelpers/integration/chainlink.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	types3 "github.com/ethereum/go-ethereum/core/types"
@@ -62,6 +63,7 @@ import (
 	"github.com/smartcontractkit/chainlink/v2/core/services/keystore/keys/ocr2key"
 	ksMocks "github.com/smartcontractkit/chainlink/v2/core/services/keystore/mocks"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/abihelpers"
+	ccipconfig "github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/config"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipdata"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipdata/v1_2_0"
 	"github.com/smartcontractkit/chainlink/v2/core/services/ocr2/plugins/ccip/internal/ccipdata/v1_5_0"
@@ -978,6 +980,42 @@ func (c *CCIPIntegrationTestHarness) SetupAndStartNodes(ctx context.Context, t *
 }
 
 func (c *CCIPIntegrationTestHarness) SetUpJobs(t *testing.T, bootstrapNode Node, configBlock uint64, jobParams CCIPJobSpecParams) {
+	// If USDCAttestationAPI is set, deploy a minimal contract on the source chain for SourceMessageTransmitterAddress
+	// This is required because LogPoller now validates that addresses are contracts
+	if jobParams.USDCAttestationAPI != "" && (jobParams.USDCConfig == nil || jobParams.USDCConfig.SourceMessageTransmitterAddress == (common.Address{})) {
+		// Deploy a minimal contract on the source chain
+		// Minimal contract bytecode: just return empty (valid contract bytecode)
+		minimalContractCode := []byte{0x60, 0x00, 0x60, 0x00, 0x52, 0x60, 0x20, 0x60, 0x00, 0xf3}
+		
+		// Get the nonce for the user
+		nonce, err := c.Source.Chain.Client().PendingNonceAt(context.Background(), c.Source.User.From)
+		require.NoError(t, err)
+		
+		// Create a transactor with the correct nonce
+		auth := *c.Source.User
+		auth.Nonce = big.NewInt(0).SetUint64(nonce)
+		
+		// Deploy the contract
+		_, tx, _, err := bind.DeployContract(&auth, abi.ABI{}, minimalContractCode, c.Source.Chain.Client())
+		require.NoError(t, err)
+		c.Source.Chain.Commit()
+		
+		// Get the contract address from the transaction receipt
+		receipt, err := c.Source.Chain.Client().TransactionReceipt(context.Background(), tx.Hash())
+		require.NoError(t, err)
+		require.Equal(t, types3.ReceiptStatusSuccessful, receipt.Status)
+		require.NotNil(t, receipt.ContractAddress)
+		
+		// Initialize USDCConfig if nil
+		if jobParams.USDCConfig == nil {
+			jobParams.USDCConfig = &ccipconfig.USDCConfig{
+				AttestationAPI:               jobParams.USDCAttestationAPI,
+				AttestationAPITimeoutSeconds: 5,
+			}
+		}
+		jobParams.USDCConfig.SourceMessageTransmitterAddress = receipt.ContractAddress
+	}
+	
 	// Add the bootstrap job
 	c.Bootstrap.AddBootstrapJob(t, jobParams.BootstrapJob(c.Dest.CommitStore.Address().Hex()))
 	c.AddAllJobs(t, jobParams)
